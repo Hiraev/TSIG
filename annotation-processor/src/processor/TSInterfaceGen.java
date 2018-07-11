9f7ab286b6ea93d812ec760bd09dd2f0004619e2
Malik <hiraev@users.noreply.github.com>
2018-07-11 20:37:08 +0300

    Extra imports were removed

--- annotation-processor/src/processor/TSInterfaceGen.java
+++ annotation-processor/src/processor/TSInterfaceGen.java
@@ -3,13 +3,14 @@ package processor;
 import annotations.TSIgnore;
 import annotations.TSInterface;
 
-import javax.annotation.processing.*;
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Messager;
+import javax.annotation.processing.ProcessingEnvironment;
+import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.TypeElement;
-import javax.lang.model.util.Elements;
-import javax.lang.model.util.Types;
 import javax.tools.Diagnostic;
 import java.io.File;
 import java.io.FileWriter;
