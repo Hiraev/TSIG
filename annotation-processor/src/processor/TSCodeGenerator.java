b6984ade51b9eed56c44f58e95421fe1bcc6d813
Malik <hiraev@users.noreply.github.com>
2018-07-15 17:10:28 +0300

    The type checker was improved

--- annotation-processor/src/processor/TSCodeGenerator.java
+++ annotation-processor/src/processor/TSCodeGenerator.java
@@ -1,6 +1,9 @@
 package processor;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 
 public class TSCodeGenerator {
 
@@ -13,38 +16,87 @@ public class TSCodeGenerator {
             builder.append("\t");
             builder.append(field.getName());
             builder.append(": ");
-            final String type = field.getType();
-            if (field.isPrimitive()) {
-                switch (type) {
-                    case "byte":
-                    case "short":
-                    case "int":
-                    case "float":
-                    case "long":
-                    case "double":
-                        builder.append("number");
-                        break;
-                    case "char":
-                        builder.append("string");
-                        break;
-                    case "boolean":
-                        builder.append("boolean");
-                }
-            } else {
-//                if (String.class.getCanonicalName().equals(type)) builder.append("string");
-//                else if (Map.class.getCanonicalName().equals(type)) builder.append("Map<Object, Object>");
-//                else if (type.startsWith(Map.class.getCanonicalName())) {
-//
-//                }
-                builder.append("Object");
-            }
+            //Check type
+            builder.append(checkType(field.getType()));
             builder.append(";\n");
         }
         builder.append("}");
         return builder.toString();
     }
-//
-//    private String checkGeneric(String string) {
-//
-//    }
+
+    private String checkType(final String type) {
+        final String primitive = checkPrimitive(type);
+        if (primitive != null) {
+            return checkPrimitive(type);
+        } else if (type.matches(".+\\]")) { //Is it an array?
+            return checkArray(type);
+        } else if (type.matches(".+>")) { //Is it a generic type?
+            return checkGeneric(type);
+        }
+        return checkObject(type);
+    }
+
+    private String checkArray(String type) {
+        int index = 0;
+        StringBuilder dimension = new StringBuilder();
+        boolean trigger = true;
+        for (int i = type.length() - 1; i >= 0; i--) {
+            final char sym = type.charAt(i);
+            if (sym != '[' && sym != ']') break;
+            index = i;
+            if (trigger) dimension.append("[]");
+            trigger = !trigger;
+        }
+        return checkType(type.substring(0, index)) + dimension.toString();
+    }
+
+    private String checkPrimitive(String type) {
+        switch (type) {
+            case "byte":
+            case "short":
+            case "int":
+            case "float":
+            case "long":
+            case "double":
+
+            case "Byte":
+            case "Short":
+            case "Integer":
+            case "Float":
+            case "Long":
+            case "Double":
+                return "number";
+            case "char":
+            case "Character":
+                return "string";
+            case "boolean":
+            case "Boolean":
+                return "boolean";
+        }
+        return null;
+    }
+
+    private String checkGeneric(String type) {
+        final int firstScope = type.indexOf('<');
+        final String basicType = checkObject(type.substring(0, firstScope));
+        if ("Object".equals(basicType)) return basicType;
+        StringBuilder innerTypes = new StringBuilder();
+        final String generic = type.substring(firstScope + 1, type.length() - 1);
+        final String[] genericTypes = generic.split(",");
+        final int lastIndex = genericTypes.length - 1;
+        for (int i = 0; i < genericTypes.length; i++) {
+            innerTypes.append(checkType(genericTypes[i].trim()));
+            if (i != lastIndex) innerTypes.append(", ");
+        }
+        return basicType + "<" + innerTypes.toString() + ">";
+    }
+
+    private String checkObject(String type) {
+        if (String.class.getCanonicalName().equals(type)) return "String";
+        else if (List.class.getCanonicalName().equals(type) ||
+                Queue.class.getCanonicalName().equals(type)) return "Array";
+        else if (Map.class.getCanonicalName().equals(type)) return "Map";
+        else if (Set.class.getCanonicalName().equals(type)) return "Set";
+        else return "Object";
+    }
 }
